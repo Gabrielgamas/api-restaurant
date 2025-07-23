e807eaeb36e6fd9e8f2e146e27f1cb3f42baba65
Gabrielgamas <gamas.enge@hotmail.com>
2025-07-23 18:29:26 -0300

    Criando migrations de Products

--- src/database/migrations/20250723211948_create-products.ts
+++ src/database/migrations/20250723211948_create-products.ts
@@ -1,10 +1,14 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.text("name").notNullable();
+    table.decimal("price").notNullable();
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("products");
 }
-
